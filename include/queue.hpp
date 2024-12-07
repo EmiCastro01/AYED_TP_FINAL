63eb6cf89296f2fc6202bc31d056534bad033a02
EmiCasttro01 <emiliano.castro@mi.unc.edu.ar>
2024-12-07 14:00:11 -0300

    queue feature

---
+++ include/queue.hpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+template <class T> class Node {
+  public:
+    Node(T data);
+    T get_data();
+    Node<T>* get_next();
+    void set_next(Node<T>* next);
+  private:
+    T data;
+    Node<T>* next;
+};
+
+template <class T> 
+Node<T>::Node(T data) {
+  this->data = data;
+  this->next = nullptr;
+}
+
+template <class T>
+T Node<T>::get_data() {
+  return this->data;
+}
+
+template <class T>
+Node<T>* Node<T>::get_next() {
+  return this->next;
+}
+
+template <class T>
+void Node<T>::set_next(Node<T>* next) {
+  this->next = next;
+}
+
+
+template <class T> class Queue {
+
+  public:
+    Queue();
+    void push(T data);
+    T pop();
+    T get_last();
+    T get_head();
+   // int size();
+    bool is_empty();
+    void print_queue();
+  
+  private:
+    int queue_size;
+    Node<T>* head;
+    Node<T>* last;
+};
+
+
+template <class T>
+Queue<T>::Queue() {
+  this->queue_size = 0;
+  this->head = nullptr;
+  this->last = nullptr;
+}
+
+template <class T>
+void Queue<T>::push(T data) {
+  Node<T>* new_node = new Node<T>(data);
+    if(this->head == nullptr) {
+      this->head = new_node;
+      this->last = this->head;
+    } else {
+      this->last->set_next(new_node);
+      this->last = new_node;
+    }
+    this->queue_size++;
+} 
+
+template <class T>
+T Queue<T>::pop() {
+  if(this->is_empty() == true) {
+    cout << "Queue is empty" << endl;
+    return 0;
+  } else {
+    Node<T>* node_temp = this->head;  // just head case
+    if(this->head == this->last) {
+      T popped_data = this->head->get_data();
+      delete this->head;
+      this->head = nullptr;
+      this->last = nullptr;
+      this->queue_size--;
+      return popped_data;
+    }
+    while (node_temp->get_next() != this->last) {
+      node_temp = node_temp->get_next();
+    }
+    this->last = node_temp;
+    T popped_data = node_temp->get_next()->get_data();
+    delete this->last->get_next();
+    this->last->set_next(nullptr);
+    this->queue_size--;
+    return popped_data;
+  }
+}
+
+template <class T>
+void Queue<T>::print_queue()
+{
+  Node<T>* node_temp = this->head;
+  cout << "[";
+  while (node_temp != nullptr) {
+    cout << node_temp->get_data() << ", ";
+    node_temp = node_temp->get_next();
+  }
+  cout << "]" << endl;
+}
+
+template <class T>
+bool Queue<T>::is_empty() {
+  return this->queue_size == 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "../include/queue.hpp"
+
 using namespace std;
 
 
 int main ()
 {
-  cout << "Initialazing the program" << endl;
+  Queue<string> q;
+  q.push("hola");
+  q.push("soy");
+  q.push("emi");
+  q.print_queue();
+  q.pop();
+  q.print_queue();
   return EXIT_SUCCESS;
 }
+
